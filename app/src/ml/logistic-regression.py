4f9ef1804e48a70d262c66c216dcefc65e0922ea
diana <dianamariamartisca@gmail.com>
2025-02-17 23:55:50 +0200

    separated output categories + trained the model

--- app/src/ml/logistic-regression.py
+++ app/src/ml/logistic-regression.py
@@ -1,10 +1,10 @@
 import json
 import numpy as np
 import re
-import os
 import tensorflow as tf
 from tensorflow.keras import preprocessing
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
 
 
 #citirea datelor
@@ -18,71 +18,88 @@ data = read_data('app/resources/data.json')
 
 #preprocesarea datelor
 all_products = []
+categories = []
 
 for category, products in data["categories"][0].items():
     if category == "alimente":
         for product in products:
-            one_product = f"{category} {product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['ingrediente']} {product['cantitate']} {product.get('descriere', '')}"
+            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['ingrediente']} {product['cantitate']} {product.get('descriere', '')}"
             all_products.append(one_product)
+            categories.append(category)
     elif category == "fashion":
         for product in products:
-            one_product = f"{category} {product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['culoare']} {product.get('descriere', '')}"
+            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['culoare']} {product.get('descriere', '')}"
             all_products.append(one_product)
+            categories.append(category)
     elif category == "electronice":
         for product in products:
-            one_product = f"{category} {product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
+            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
             all_products.append(one_product)
+            categories.append(category)
     else:
         for product in products:
-            one_product = f"{category} {product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
+            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
             all_products.append(one_product)
+            categories.append(category)
 
 for index in range(len(all_products)):
     all_products[index] = re.sub(r'[^\w\s]','',all_products[index])
-#all_products = ["1 " + produs for produs in all_products]
-#print(all_products)
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(all_products)
 tokenized_sequences = tokenizer.texts_to_sequences(all_products)
 #print(tokenized_sequences)
-
 max_input_length = max(len(seq) for seq in tokenized_sequences)
 #print(f'Combined max sequence length is {max_input_length}')
-
 padded_sequences = preprocessing.sequence.pad_sequences(tokenized_sequences, maxlen=max_input_length, padding='post')
 input_data = np.array(padded_sequences)
 bias_column = np.ones((input_data.shape[0], 1))  # Column of ones
 input_data = np.hstack((bias_column, input_data))
 print(f'Input data shape -> {input_data.shape}')
 #print(input_data)
-
 input_word_dict = tokenizer.word_index
 num_input_tokens = len( input_word_dict )+1
-print('Number of input tokens = {}'.format( num_input_tokens))
+#print('Number of input tokens = {}'.format( num_input_tokens))
 #print(input_word_dict)
+
+unique_entries = list(set(categories))
+output_word_dict = {entry: index for index, entry in enumerate(unique_entries)}
+tokenized_categories = [[output_word_dict[entry]] for entry in categories]
+max_output_length = max(len(token_seq) for token_seq in tokenized_categories)
+#print('Output max length is {}'.format(max_output_length))
+output_data = np.array(tokenized_categories)
+print('Output data shape -> {}'.format(output_data.shape))
+#print(output_data)
+num_output_tokens = len(output_word_dict) 
+#print('Number of output tokens = {}'.format(num_output_tokens))
+#print(output_word_dict)
 
 
 #prepararea datelor
 p = np.random.permutation(len(input_data))
 input_data = input_data[p]
 
-train_size = int(0.6 * len(input_data))
-val_size = int(0.2 * len(input_data))
-test_size = len(input_data) - train_size - val_size
+train_size = int(0.8 * len(input_data))
+test_size = len(input_data) - train_size 
 
 train_input = input_data[:train_size]
-validation_input = input_data[train_size:train_size + val_size]
-test_input = input_data[train_size + val_size:]
+test_input = input_data[train_size:]
 
 print('Train input shape:', train_input.shape)
-print('Validation input shape:', validation_input.shape)
 print('Test input shape:', test_input.shape)
+
+train_output = output_data[:train_size]
+test_output = output_data[train_size:]
+
+print('Train output shape:', train_output.shape)
+print('Test output shape:', test_output.shape)
 
 
 #functia sigmoid
 def sigm(x):
-    return 1/(1+np.exp(-x))
+    x_clipped = np.clip(x, -500, 500)
+    return 1/(1+np.exp(-x_clipped))
+
 
 #functia softmax
 def softmax(z):
@@ -94,7 +111,7 @@ def softmax(z):
 def gradient_descent(X, y, theta, learning_rate, iterations):
 
     # X: matricea caracteristicilor (m x n)
-    # y: vectorul țintă (m x 1) cu valori 0 sau 1
+    # y: vectorul țintă (m x num_classes) cu valori 0 sau 1
     # theta: vectorul inițial al parametrilor (n x 1)
 
     m = len(y)  
@@ -107,6 +124,7 @@ def gradient_descent(X, y, theta, learning_rate, iterations):
 
     return theta  
 
+
 #functia train
 def train(X, y, num_classes, learning_rate=0.01, iterations=1000):
     m, n = X.shape  
@@ -115,9 +133,12 @@ def train(X, y, num_classes, learning_rate=0.01, iterations=1000):
     for c in range(num_classes):
         y_c = (y == c).astype(int)  
         theta = np.random.randn(n, 1)  
-        all_theta[:, c] = gradient_descent(X, y_c, theta, learning_rate, iterations)
+        all_theta[:, c] = gradient_descent(X, y_c, theta, learning_rate, iterations).flatten()
 
     return all_theta  
+
+all_theta = train(train_input, train_output, num_output_tokens)
+#print("Shape of all_theta:", all_theta.shape)
 
 #functia predict
 def predict_multiclass(X, all_theta):
