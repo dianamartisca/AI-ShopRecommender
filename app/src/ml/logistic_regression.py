6002f71f845da61904c86811878074eda9f81f51
diana <dianamariamartisca@gmail.com>
2025-02-19 20:15:33 +0200

    implemented chatbot

--- app/src/ml/logistic_regression.py
+++ app/src/ml/logistic_regression.py
@@ -4,7 +4,7 @@ import re
 import tensorflow as tf
 from tensorflow.keras import preprocessing
 from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
 #citirea datelor
@@ -12,86 +12,93 @@ def read_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
     return data
-     
-data = read_data('app/resources/data.json')
 
 
 #preprocesarea datelor
-all_products = []
-categories = []
+def extract_data(data):
+    all_products = []
+    categories = []
 
-for category, products in data["categories"][0].items():
-    if category == "alimente":
-        for product in products:
-            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['ingrediente']} {product['cantitate']} {product.get('descriere', '')}"
-            all_products.append(one_product)
-            categories.append(category)
-    elif category == "fashion":
-        for product in products:
-            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['culoare']} {product.get('descriere', '')}"
-            all_products.append(one_product)
-            categories.append(category)
-    elif category == "electronice":
-        for product in products:
-            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
-            all_products.append(one_product)
-            categories.append(category)
-    else:
-        for product in products:
-            one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
-            all_products.append(one_product)
-            categories.append(category)
+    for category, products in data["categories"][0].items():
+        if category == "alimente":
+            for product in products:
+                one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['ingrediente']} {product['cantitate']} {product.get('descriere', '')}"
+                all_products.append(one_product)
+                categories.append(category)
+        elif category == "fashion":
+            for product in products:
+                one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product['culoare']} {product.get('descriere', '')}"
+                all_products.append(one_product)
+                categories.append(category)
+        elif category == "electronice":
+            for product in products:
+                one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
+                all_products.append(one_product)
+                categories.append(category)
+        else:
+            for product in products:
+                one_product = f"{product['nume_produs']} {product['nume_producator']} {product['pret']} RON {product['rating']} rating {product.get('descriere', '')}"
+                all_products.append(one_product)
+                categories.append(category)
+    
+    return all_products, categories
 
-for index in range(len(all_products)):
-    all_products[index] = re.sub(r'[^\w\s]','',all_products[index])
+def preprocess_input(all_products):
+    for index in range(len(all_products)):
+        all_products[index] = re.sub(r'[^\w\s]','',all_products[index])
 
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(all_products)
-tokenized_sequences = tokenizer.texts_to_sequences(all_products)
-#print(tokenized_sequences)
-max_input_length = max(len(seq) for seq in tokenized_sequences)
-#print(f'Combined max sequence length is {max_input_length}')
-padded_sequences = preprocessing.sequence.pad_sequences(tokenized_sequences, maxlen=max_input_length, padding='post')
-input_data = np.array(padded_sequences)
-bias_column = np.ones((input_data.shape[0], 1))  # Column of ones
-input_data = np.hstack((bias_column, input_data))
-print(f'Input data shape -> {input_data.shape}')
-input_word_dict = tokenizer.word_index
-num_input_tokens = len( input_word_dict )+1
-#print('Number of input tokens = {}'.format( num_input_tokens))
-#print(input_word_dict)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(all_products)
+    tokenized_sequences = tokenizer.texts_to_sequences(all_products)
+    #print(tokenized_sequences)
+    max_input_length = max(len(seq) for seq in tokenized_sequences)
+    #print(f'Combined max sequence length is {max_input_length}')
+    padded_sequences = preprocessing.sequence.pad_sequences(tokenized_sequences, maxlen=max_input_length, padding='post')
+    input_data = np.array(padded_sequences)
+    bias_column = np.ones((input_data.shape[0], 1))  # Column of ones
+    input_data = np.hstack((bias_column, input_data))
+    #print(f'Input data shape -> {input_data.shape}')
+    input_word_dict = tokenizer.word_index
+    #num_input_tokens = len( input_word_dict )+1
+    #print('Number of input tokens = {}'.format( num_input_tokens))
+    #print(input_word_dict)
 
-unique_entries = list(set(categories))
-output_word_dict = {entry: index for index, entry in enumerate(unique_entries)}
-tokenized_categories = [[output_word_dict[entry]] for entry in categories]
-max_output_length = max(len(token_seq) for token_seq in tokenized_categories)
-#print('Output max length is {}'.format(max_output_length))
-output_data = np.array(tokenized_categories)
-print('Output data shape -> {}'.format(output_data.shape))
-#print(output_data)
-num_output_tokens = len(output_word_dict) 
-#print('Number of output tokens = {}'.format(num_output_tokens))
-print(output_word_dict)
+    return input_data, max_input_length, input_word_dict
+
+def preprocess_output(categories):
+    unique_entries = list(set(categories))
+    output_word_dict = {entry: index for index, entry in enumerate(unique_entries)}
+    tokenized_categories = [[output_word_dict[entry]] for entry in categories]
+    #max_output_length = max(len(token_seq) for token_seq in tokenized_categories)
+    #print('Output max length is {}'.format(max_output_length))
+    output_data = np.array(tokenized_categories)
+    #print('Output data shape -> {}'.format(output_data.shape))
+    #print(output_data)
+    num_output_tokens = len(output_word_dict) 
+    #print('Number of output tokens = {}'.format(num_output_tokens))
+    #print(output_word_dict)
+
+    return output_data, output_word_dict, num_output_tokens
 
 
-#prepararea datelor
-p = np.random.permutation(len(input_data))
-input_data = input_data[p]
+#separarea datelor
+def split_data(input_data, output_data):
+    p = np.random.permutation(len(input_data))
+    input_data = input_data[p]
 
-train_size = int(0.8 * len(input_data))
-test_size = len(input_data) - train_size 
+    train_size = int(0.8 * len(input_data))
 
-train_input = input_data[:train_size]
-test_input = input_data[train_size:]
+    train_input = input_data[:train_size]
+    test_input = input_data[train_size:]
+    #print('Train input shape:', train_input.shape)
+    #print('Test input shape:', test_input.shape)
 
-print('Train input shape:', train_input.shape)
-print('Test input shape:', test_input.shape)
+    train_output = output_data[:train_size]
+    test_output = output_data[train_size:]
+    #print('Train output shape:', train_output.shape)
+    #print('Test output shape:', test_output.shape)
 
-train_output = output_data[:train_size]
-test_output = output_data[train_size:]
-
-print('Train output shape:', train_output.shape)
-print('Test output shape:', test_output.shape)
+    return train_input, test_input, train_output, test_output
 
 
 #functia sigmoid
@@ -136,12 +143,38 @@ def train(X, y, num_classes, learning_rate=0.01, iterations=1000):
 
     return all_theta  
 
-all_theta = train(train_input, train_output, num_output_tokens)
-#print("Shape of all_theta:", all_theta.shape)
+def train_model():
+    data = read_data('app/resources/data.json')
+    all_products, categories = extract_data(data)
+    input_data, max_input_length, input_word_dict = preprocess_input(all_products)
+    output_data, output_word_dict, num_output_tokens = preprocess_output(categories)
+    train_input, test_input, train_output, test_output = split_data(input_data, output_data)
+    all_theta = train(train_input, train_output, num_output_tokens)
+    #print("Shape of all_theta:", all_theta.shape)
+
+    return all_theta, input_word_dict, output_word_dict, max_input_length
+
+all_theta, input_word_dict, output_word_dict, max_input_length = train_model()
 
 #functia predict
 def predict_multiclass(X):
     z = X @ all_theta  
-    p = softmax(z) 
-    print("Softmax probabilities:", p)
-    return np.argmax(p, axis=1).item()
+    probabilities = softmax(z) 
+    return np.argmax(probabilities, axis=1).item()
+
+def predict(text):
+    text = re.sub(r'[^\w\s]','',text)
+    words = text.split()
+    words = [word.lower() for word in words]
+    tokenized_text = [[input_word_dict[entry]] for entry in words]
+    tokenized_text = pad_sequences([tokenized_text], maxlen=max_input_length, padding='post')   
+    X = tokenized_text.flatten().reshape(1, max_input_length)
+    X = np.hstack((np.ones((X.shape[0], 1)), X))
+    #print(X.shape)
+    #print(X)
+    category_index = predict_multiclass(X)
+    index_to_category = {index: category for category, index in output_word_dict.items()}
+    category = index_to_category.get(category_index, "Unknown")
+    #print(category)
+    #print(category_index)
+    return category
--- main.py
+++ main.py
@@ -1,17 +1,51 @@
 import os
 import json
+import threading
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import random
-import re
-import numpy as np
-from app.src.ml.logistic_regression import predict_multiclass, input_word_dict, output_word_dict, max_input_length
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import warnings
+import tensorflow as tf
+from transformers import pipeline
+from transformers.utils import logging
+from app.src.ml.logistic_regression import predict
+#from sentence_transformers import SentenceTransformer, util
 
+# Suppress all TensorFlow warnings
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  
+tf.get_logger().setLevel("ERROR")  
+
+# Suppress all warnings from Python and Transformers
+warnings.filterwarnings("ignore")
+logging.set_verbosity_error()
 
 with open('app/resources/data.json', 'r', encoding='utf-8') as f:
 	data = json.load(f)
+     
+def extract_products(data):
+    all_products = []
+
+    for category, products in data["categories"][0].items():
+        for product in products:
+            product_id = product["id"]
+            product_name = product["nume_produs"]
+
+            details = f"{product['nume_producator']} {product['pret']} RON {product['rating']} rating"
+        
+            if category == "alimente":
+                details += f" {product['ingrediente']} {product['cantitate']} {product.get('descriere', '')}"
+            elif category == "fashion":
+                details += f" {product['culoare']} {product.get('descriere', '')}"
+            else: 
+                details += f" {product.get('descriere', '')}"
+        
+            all_products.append({
+                "id": product_id,
+                "nume_produs": product_name,
+                "rest": details.strip()
+            })
+    return all_products
 
 def load_image(file_path, x, y):
     try:
@@ -36,8 +70,9 @@ def add_to_cart(product_id):
     popup = tk.Toplevel()
     popup.title("Added to Cart")
     popup.geometry("1000x700")
-    popup.configure(bg="#2F4F4F")  # Fundal pentru fereastra pop-up
+    popup.configure(bg="#2F4F4F")  
 
+    all_products = extract_products(data)
     for product in all_products:
         if product["id"] == product_id:
             product_name = product["nume_produs"]
@@ -49,31 +84,18 @@ def add_to_cart(product_id):
     label = tk.Label(popup, text=f"Produsul '{product_name}' a fost adăugat în coș!", font=("Georgia", 15, "bold"), pady=10, bg="#2F4F4F", fg="white")
     label.pack()
 
-    text = re.sub(r'[^\w\s]','',text)
-    words = text.split()
-    words = [word.lower() for word in words]
-    tokenized_text = [[input_word_dict[entry]] for entry in words]
-    tokenized_text = pad_sequences([tokenized_text], maxlen=max_input_length, padding='post')   
-    X = tokenized_text.flatten().reshape(1, max_input_length)
-    X = np.hstack((np.ones((X.shape[0], 1)), X))
-    #print(X.shape)
-    #print(X)
-    category_index = predict_multiclass(X)
-    index_to_category = {index: category for category, index in output_word_dict.items()}
-    category = index_to_category.get(category_index, "Unknown")
-    #print(category)
-    #print(category_index)
-
     # Select 3 random recommended products from the predicted category
+    category = predict(text)
     categories = data["categories"][0] 
     products = categories[category]
-    recommended = random.sample(products, min(3, len(products)))
+    filtered_products = [p for p in products if p["id"] != product_id]
+    recommended = random.sample(filtered_products, min(3, len(filtered_products)))
 
     style = ttk.Style()
     style.configure("Custom.TFrame", background="#2F4F4F")
 
     # Frame pentru produsele recomandate
-    rec_frame = ttk.Frame(popup, style="Custom.TFrame")  # Folosim un Frame simplu, fără LabelFrame
+    rec_frame = ttk.Frame(popup, style="Custom.TFrame")  
     rec_frame.pack(fill="both", padx=10, pady=5, expand=True)
 
     # Text pentru recomandări
@@ -81,7 +103,7 @@ def add_to_cart(product_id):
     recommendation_label.pack(fill='x', pady=5)  
 
     for product in recommended:
-        product_frame = ttk.Frame(rec_frame, style="Custom.TFrame")  # Fundal
+        product_frame = ttk.Frame(rec_frame, style="Custom.TFrame") 
         product_frame.pack(fill='x', pady=5)
 
         img_path = os.path.join("app/images", os.path.basename(product["imagine"]))
@@ -105,13 +127,12 @@ def add_to_cart(product_id):
 
         cart_icon = load_image("app/images/cos-de-cumparaturi.png", 40, 40)
         if cart_icon:
-            cart_button = tk.Button(product_frame, image=cart_icon, command=lambda p=product["nume_produs"]: add_to_cart(p), bg="#2F4F4F", borderwidth=0)
+            cart_button = tk.Button(product_frame, image=cart_icon, command=lambda p=product["id"]: add_to_cart(p), bg="#2F4F4F", borderwidth=0)
             cart_button.image = cart_icon
             cart_button.pack(side='right', padx=10)
 
     close_button = ttk.Button(popup, text="OK", command=popup.destroy)
     close_button.pack(pady=10)
-
 
 def display_products(parent, category, products):
     frame = ttk.Frame(parent, padding=10, style="Custom.TFrame")  
@@ -119,8 +140,7 @@ def display_products(parent, category, products):
 
     # Textul pentru categoria respectivă, aliniat la stânga
     label = tk.Label(frame, text=category, font=("Georgia", 15, "bold"), bg="#2F4F4F", fg="white", anchor='w')
-    label.pack(fill='x', pady=5)  # Aliniat la stânga (anchor='w')
-
+    label.pack(fill='x', pady=5)  
 
     for product in products:
         product_frame = ttk.Frame(frame, style="Custom.TFrame")  
@@ -129,7 +149,7 @@ def display_products(parent, category, products):
         image_path = os.path.join("app/images", os.path.basename(product["imagine"]))
         image = load_image(image_path, 150, 150)
 
-        img_label = tk.Label(product_frame, image=image, bg="#2F4F4F")  # Fundal la imagine
+        img_label = tk.Label(product_frame, image=image, bg="#2F4F4F")  
         img_label.image = image
         img_label.pack(side='left', padx=10)
 
@@ -142,16 +162,15 @@ def display_products(parent, category, products):
         else:
             text = f"{product['nume_produs']}\n{product['nume_producator']}\n{product['pret']} RON\nRating: {product['rating']}\n{product['descriere']}"    
 
-        text_label = tk.Label(product_frame, text=text, justify='left', anchor='w', bg="#2F4F4F", fg="white", font=("Georgia", 10))  # Fundal la text
+        text_label = tk.Label(product_frame, text=text, justify='left', anchor='w', bg="#2F4F4F", fg="white", font=("Georgia", 10))  
         text_label.pack(side='left', padx=10)
 
         # Iconiță coș de cumpărături
-        cart_icon = load_image("app/images/cos-de-cumparaturi.png", 40, 40)  # Iconița de coș
+        cart_icon = load_image("app/images/cos-de-cumparaturi.png", 40, 40)  
         if cart_icon:
             cart_button = tk.Button(product_frame, image=cart_icon, command=lambda p=product["id"]: add_to_cart(p), bg="#2F4F4F", borderwidth=0)
             cart_button.image = cart_icon
             cart_button.pack(side='right', padx=10)
-
 
 def on_mouse_wheel(event, canvas):
 	if event.delta > 0:
@@ -159,12 +178,23 @@ def on_mouse_wheel(event, canvas):
 	else:
 		canvas.yview_scroll(1, "units")
 
+def chatbot():
+    print("Eu sunt asistentul tău virtual. Cu ce te pot ajuta?")
+    while True:
+        question = input("Întrebare: ")
+        qa_pipeline = pipeline("question-answering", model="deepset/roberta-base-squad2")
+        context = "The iPhone 15 Pro Max has a titanium frame and offers up to 29 hours of battery life."
+        result = qa_pipeline(question=question, context=context)
+        print("Răspuns:", result["answer"]) 
+
+chatbot_thread = threading.Thread(target=chatbot, daemon=True)
+chatbot_thread.start()
+
 root = tk.Tk()
 root.title("Catalog Produse")
 root.geometry("1250x700")
 root.configure(bg="#2F4F4F")
 
-# Define style for frame and add layout
 style = ttk.Style()
 style.configure("Custom.TFrame", background="#2F4F4F")
 
@@ -172,7 +202,7 @@ style.configure("Custom.TFrame", background="#2F4F4F")
 canvas = tk.Canvas(root, bg="#2F4F4F")
 scrollbar = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
 
-scroll_frame = ttk.Frame(canvas, style="Custom.TFrame")  # Aplicăm stilul
+scroll_frame = ttk.Frame(canvas, style="Custom.TFrame")  
 scroll_frame.bind(
     "<Configure>",
     lambda e: canvas.configure(
@@ -190,28 +220,6 @@ scrollbar.pack(side="right", fill="y")
 for category, products in data["categories"][0].items():
     display_products(scroll_frame, category.capitalize(), products)
 
-all_products = []
-
-for category, products in data["categories"][0].items():
-    for product in products:
-        product_id = product["id"]
-        product_name = product["nume_produs"]
-
-        details = f"{product['nume_producator']} {product['pret']} RON {product['rating']} rating"
-        
-        if category == "alimente":
-            details += f" {product['ingrediente']} {product['cantitate']} {product.get('descriere', '')}"
-        elif category == "fashion":
-            details += f" {product['culoare']} {product.get('descriere', '')}"
-        else: 
-            details += f" {product.get('descriere', '')}"
-        
-        all_products.append({
-            "id": product_id,
-            "nume_produs": product_name,
-            "rest": details.strip()
-        })
-
-
 root.bind_all("<MouseWheel>", lambda event: on_mouse_wheel(event, canvas))
 root.mainloop()
+
